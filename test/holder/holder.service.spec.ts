8a085402993f55b6c38bdc8f079e564cbb17c663
Ashar Oran <asharoran@Ashars-MacBook-Pro.local>
2024-11-10 10:08:08 +0300

    setup the test files

---
+++ test/holder/holder.service.spec.ts
@@ -0,0 +1,6 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { HolderService } from "src/modules/holder/service/holder.service";
+
+
+//create holder
+//response in event
---
+++ test/issuer/issuer.service.spec.ts
@@ -0,0 +1,4 @@
+import { Test , TestingModule} from "@nestjs/testing";
+import { IssuerService } from "src/modules/issuer/service/issuer.service";
+
+//create issuer
